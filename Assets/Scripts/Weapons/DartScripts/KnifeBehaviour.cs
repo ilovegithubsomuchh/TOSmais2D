1803c22e6c4d339336fef1417696885c50e8df5f
ilovegithubsomuchh <49435669+ilovegithubsomuchh@users.noreply.github.com>
2024-01-03 15:09:56 +0100

    Sprite rotation

--- Assets/Scripts/Weapons/DartScripts/KnifeBehaviour.cs
+++ Assets/Scripts/Weapons/DartScripts/KnifeBehaviour.cs
@@ -1,50 +1,77 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Cinemachine.Utility;
 using UnityEngine;
 
 public class KnifeBehaviour : WeaponManager
 {
     public PlayerMovement PlayerMovement;
     private float WeaponDirectionX;
-    public float WeaponDirectionY;
-
+    private float WeaponDirectionY;
+    private SpriteRenderer _spriteRenderer;
 
     protected override void Start()
     {
         base.Start();
 
+        // Find and store the PlayerMovement component
         PlayerMovement = FindObjectOfType<PlayerMovement>();
+
+        // Get the SpriteRenderer component attached to this GameObject
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // Initialize WeaponDirectionX and WeaponDirectionY with PlayerMovement input values
         WeaponDirectionX = PlayerMovement._inputValueX;
         WeaponDirectionY = PlayerMovement._inputValueY;
-        if ((WeaponDirectionX == 0) && (WeaponDirectionY == 0))
+
+        // If both input values are approximately 0, set WeaponDirectionX to 1
+        if (Mathf.Approximately(WeaponDirectionX, 0) && Mathf.Approximately(WeaponDirectionY, 0))
         {
             WeaponDirectionX = 1;
         }
 
+        // Initialize _destroy to true
         _destroy = true;
     }
 
     protected override void Update()
     {
+        // Call the Attack method
         Attack();
+
+        // If _destroy is true, increment TimeBeforeDestroy with deltaTime
         if (_destroy) TimeBeforeDestroy += Time.deltaTime;
+
+        // If TimeBeforeDestroy exceeds TimeBeforeDestruction, call the Destroy method
         if (TimeBeforeDestroy > WeaponData.TimeBeforeDestruction) Destroy();
     }
 
     protected override void Attack()
     {
-        transform.position += new Vector3((WeaponDirectionX * (WeaponData.speed * Time.deltaTime)),
-            (WeaponDirectionY * (WeaponData.speed * Time.deltaTime)), 0);
+        // Calculate the movement vector based on WeaponDirectionX and WeaponDirectionY
+        Vector3 movement = new Vector3(WeaponDirectionX * (WeaponData.speed * Time.deltaTime),
+            WeaponDirectionY * (WeaponData.speed * Time.deltaTime), 0);
+
+        // Update the position of the GameObject
+        transform.position += movement;
+
+        // Calculate rotation angle based on movement direction
+        float angle = Mathf.Atan2(WeaponDirectionX, WeaponDirectionY) * Mathf.Rad2Deg;
+
+        // Apply rotation to the object on the z-axis
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        // If WeaponDirectionY is not zero, flip the sprite vertically
+        if (WeaponDirectionY != 0)
+        {
+            _spriteRenderer.flipY = true;
+        }
+        else
+        {
+            _spriteRenderer.flipY = false;
+        }
     }
 
     protected override void Destroy()
     {
+        // Destroy the GameObject
         Destroy(gameObject);
     }
-   
-    
-
-    
 }
